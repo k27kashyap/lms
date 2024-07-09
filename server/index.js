fddedb1fc9a91ca440036924ff385789b4bb1cb3
Khushi <k27kashyap@gmail.com>
2024-07-09 20:10:04 +0530

    index.js

--- server/index.js
+++ server/index.js
@@ -9,12 +9,14 @@ import { bookRouter } from './routes/book.js';
 import { Book } from './models/Book.js';
 import { Student } from './models/Student.js';
 import { Admin } from './models/Admin.js';
+const RegisterModel = require('./models/Register');
 
 const app = express();
 app.use(express.json());
 app.use(
     cors({
-        origin: ['http://localhost:5173'],
+        origin: ['https://deploy-mern-1whq.vercel.app'],
+        methods: ['PUT', 'POST', 'GET', 'DELETE'],
         credentials: true,
     })
 );
